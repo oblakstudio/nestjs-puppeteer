aad6b1cb08acbff7901771b751e2816761a8245d
Sibin Grasic <sibin.grasic@oblak.studio>
2024-01-31 15:40:39 +0100

    chore(build): Auto-build fix

--- .github/scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NEXT_VERSION=$1
-CURRENT_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
-
-sed -i "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEXT_VERSION\"/g" package.json
-
-zip -qr /tmp/release.zip dist package.json README.md
